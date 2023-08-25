18dc37910c161d1a01160cc883d6d42d10d9fcf8
Grand Nabil <akambilawani@gmail.com>
2023-08-26 00:31:28 +0100

    new models

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+import datetime
+from django.db import models
+from django.utils import timezone
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -16,3 +20,9 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+class Question(models.Model):
+    # ...
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
